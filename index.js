a9a2cd7270660a98ef251f8ec2a78bc8956c9922
NisarCoderHive <nisarapt@gmail.com>
2021-07-23 10:20:32 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ const app = new express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/index.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
-});
+}); 
+ 
+ 
+ 
+ 
+ 
+
